49f154df5ce6fce7da396284d09cc7e848d0f08b
Dan Luo <luo_dana@yahoo.com>
2023-04-17 21:30:52 +1000

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -43,43 +43,43 @@ def check_sig(payload,sig):
 def fill_order(order,txes=[]):
     #Your code here
     #if all(key in order for key in ['sender_pk','buy_amount','sell_amount', 'receiver_pk', 'buy_currency', 'sell_currency']):
-        order_obj = order
-        
-        for existing_order in session.query(Order).all():
-            if order_obj.sell_amount * existing_order.sell_amount >= order_obj.buy_amount * existing_order.buy_amount and existing_order.buy_currency == order.sell_currency and existing_order.sell_currency == order.buy_currency and existing_order.filled == None:
-                order_obj.filled = func.now()
-                existing_order.filled = func.now()
-                order_obj.counterparty_id = existing_order.id
-                existing_order.counterparty_id = order_obj.id
-                if order_obj['buy_amount']>existing_order['sell_amount']:
-                    order_r = {}
-                    order_r['filled'] = None
-                    order_r['creator_id'] = order_obj['id']
-                    order_r['sender_pk'] = order_obj['sender_pk']
-                    order_r['receiver_pk'] = order_obj['receiver_pk']
-                    order_r['buy_currency'] = order_obj['buy_currency']
-                    order_r['sell_currency'] = order_obj['sell_currency']
-                    order_r['buy_amount'] = order_obj['buy_amount']-existing_order['sell_amount']
-                    order_r['sell_amount'] = order_obj['sell_amount']-existing_order['buy_amount']
-                    session.add(order_r)
-                    session.add(order_obj)
-                elif existing_order['buy_amount']>order_obj['sell_amount']:
-                    order_r = {}
-                    order_r['filled'] = None
-                    order_r['creator_id'] = existing_order['id']
-                    order_r['sender_pk'] = existing_order['sender_pk']
-                    order_r['receiver_pk'] = existing_order['receiver_pk']
-                    order_r['buy_currency'] = existing_order['buy_currency']
-                    order_r['sell_currency'] = existing_order['sell_currency']
-                    order_r['buy_amount'] = existing_order['buy_amount']-order_obj['sell_amount']
-                    order_r['sell_amount'] = existing_order['sell_amount']-order_obj['buy_amount']
-                    session.add(order_r)
-                    session.add(order_obj)                            
-            g.session.commit()
-            txes.append(order_obj)
-            txes.append(existing_order)
-            break
-        return txes
+    order_obj = order
+    
+    for existing_order in session.query(Order).all():
+        if order_obj.sell_amount * existing_order.sell_amount >= order_obj.buy_amount * existing_order.buy_amount and existing_order.buy_currency == order.sell_currency and existing_order.sell_currency == order.buy_currency and existing_order.filled == None:
+            order_obj.filled = func.now()
+            existing_order.filled = func.now()
+            order_obj.counterparty_id = existing_order.id
+            existing_order.counterparty_id = order_obj.id
+            if order_obj['buy_amount']>existing_order['sell_amount']:
+                order_r = {}
+                order_r['filled'] = None
+                order_r['creator_id'] = order_obj['id']
+                order_r['sender_pk'] = order_obj['sender_pk']
+                order_r['receiver_pk'] = order_obj['receiver_pk']
+                order_r['buy_currency'] = order_obj['buy_currency']
+                order_r['sell_currency'] = order_obj['sell_currency']
+                order_r['buy_amount'] = order_obj['buy_amount']-existing_order['sell_amount']
+                order_r['sell_amount'] = order_obj['sell_amount']-existing_order['buy_amount']
+                session.add(order_r)
+                session.add(order_obj)
+            elif existing_order['buy_amount']>order_obj['sell_amount']:
+                order_r = {}
+                order_r['filled'] = None
+                order_r['creator_id'] = existing_order['id']
+                order_r['sender_pk'] = existing_order['sender_pk']
+                order_r['receiver_pk'] = existing_order['receiver_pk']
+                order_r['buy_currency'] = existing_order['buy_currency']
+                order_r['sell_currency'] = existing_order['sell_currency']
+                order_r['buy_amount'] = existing_order['buy_amount']-order_obj['sell_amount']
+                order_r['sell_amount'] = existing_order['sell_amount']-order_obj['buy_amount']
+                session.add(order_r)
+                session.add(order_obj)                            
+        g.session.commit()
+        txes.append(order_obj)
+        txes.append(existing_order)
+        break
+    return txes
   
 def log_message(d):
     # Takes input dictionary d and writes it to the Log table
